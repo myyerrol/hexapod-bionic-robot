a724ffe70df8f78dbe8d8c967becc0c012868db8
myyerrol <myyerrol@126.com>
2016-10-05 16:00:20 +0800

    Delete the serial's test

--- examples/hexapod_bionic_robot_test/hexapod_bionic_robot_test.ino
+++ examples/hexapod_bionic_robot_test/hexapod_bionic_robot_test.ino
@@ -1,38 +1,26 @@
 #include <hexapod_bionic_robot.h>
 
-char               g_char = 0;
+float              g_distance = 0.0;
 HexapodBionicRobot g_hexapod_bionic_robot;
 
 void setup()
 {
     Serial.begin(9600);
-    pinMode(13, OUTPUT);
 }
 
 void loop()
 {
-    // float distance = g_hexapod_bionic_robot.getDistance();
+    g_distance = g_hexapod_bionic_robot.getDistance();
 
-    // if (distance <= 5.0) {
-    //     digitalWrite(13, HIGH);
-    // }
-    // else {
-    //     digitalWrite(13, LOW);
-    // }
-
-    // Serial.print("Distance: ");
-    // Serial.println(g_hexapod_bionic_robot.getDistance());
-    // Serial.print("cm");
-
-    if (Serial.available()) {
-        g_char = Serial.read();
-        if (g_char == 'a') {
-            digitalWrite(13, HIGH);
-            Serial.println("Pin On!");
-        }
-        else if (g_char == 'b') {
-            digitalWrite(13, LOW);
-            Serial.println("Pin Off!");
-        }
+    if (g_distance <= 5.0) {
+        digitalWrite(PIN_LED, HIGH);
+        Serial.println("Warning, Distance is too close!!!");
     }
+    else {
+        digitalWrite(PIN_LED, LOW);
+    }
+
+    Serial.print(g_distance);
+    Serial.println("cm");
+    delay(100);
 }
